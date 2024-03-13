50a85d88cb14ebc4bd6fdedcccfdac6b444f522c
Robay20 <162143073+Robay20@users.noreply.github.com>
2024-03-13 17:39:49 +0000

    hak ha l jadid

--- src/main/java/com/example/gestiontrip/repository/TripPlanifieReposirory.java
+++ src/main/java/com/example/gestiontrip/repository/TripPlanifieReposirory.java
@@ -1,7 +1,7 @@
 package com.example.gestiontrip.repository;
 
-import com.example.gestiontrip.model.Trajetplaifie;
+import com.example.gestiontrip.model.TrajetProgrammer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TripPlanifieReposirory extends CrudRepository<Trajetplaifie,Long> {
+public interface TripPlanifieReposirory extends CrudRepository<TrajetProgrammer,Long> {
 }
