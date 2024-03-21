1c759b81af877f16a33d3a104643a7d86b11bf33
Yassine_K <kacimiyassine16@gmail.com>
2024-03-21 11:18:59 +0000

    improving models and still not lanced fix them it your job not mean lmao

--- src/main/java/com/example/gestiontrip/service/DiponibleService.java
+++ src/main/java/com/example/gestiontrip/service/DiponibleService.java
@@ -1,4 +1,3 @@
-/*
 package com.example.gestiontrip.service;
 
 import com.example.gestiontrip.model.Conducteur;
@@ -10,22 +9,12 @@ import com.example.gestiontrip.repository.VehiculeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
 public class DiponibleService {
-    private final TrajetProgrammerRepository trajetProgrammerRepository;
-    private final ConducteurRepository conducteurRepository;
-    private final VehiculeRepository vehiculeRepository;
-
-    @Autowired
-    public DiponibleService(TrajetProgrammerRepository trajetProgrammerRepository, ConducteurRepository conducteurRepository, VehiculeRepository vehiculeRepository) {
-        this.trajetProgrammerRepository = trajetProgrammerRepository;
-        this.conducteurRepository = conducteurRepository;
-        this.vehiculeRepository = vehiculeRepository;
-    }
-
-    public boolean DisponibleVehicule(Long vehiculeId){
+    /*public boolean DisponibleVehicule(Long vehiculeId){
         //boolean ruleVehicule=false;
         Optional<Vehicule> vehi = vehiculeRepository.findById(vehiculeId);
         if(vehi.isPresent()) {
@@ -42,8 +31,8 @@ public class DiponibleService {
             return conducteur.isDisponibilite();
         }
         return false;
-    }
-    public boolean DisponibleTrajetPlanifier(Long trajetProgrammerId){
+    }*/
+    public boolean isSufficientSeatsAvailable(LocalDate dateArriveePrevue, LocalDate dateDepart){
         boolean rule1= false;
         boolean rule2= false;
         Optional<TrajetProgrammer> trajet = trajetProgrammerRepository.findById(trajetProgrammerId);
@@ -61,5 +50,17 @@ public class DiponibleService {
         }
     return rule1 && rule2;
     }
+    public boolean isDateTrajetValide(LocalDate dateArriveePrevue, LocalDate dateDepart){
+            if (dateArriveePrevue.compareTo(dateDepart) > 0)
+                return true;
+            return false;
+            Optional<Vehicule> vehicule = vehiculeRepository.findById(trajetProgrammer.getIdVehicule());
+            if (vehicule.isPresent()){
+                Vehicule vehicule1=vehicule.get();
+                if (vehicule1.getNbPlace() > trajetProgrammer.getNbPassagers())
+                    rule2=true;
+            }
+        }
+        return rule1 && rule2;
+    }
 }
-*/
