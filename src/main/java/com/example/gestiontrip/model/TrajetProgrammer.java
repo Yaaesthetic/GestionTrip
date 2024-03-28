4f2599777d65716b933ff03148b8cafbaaa9b52f
Yassine_K <kacimiyassine16@gmail.com>
2024-03-28 11:55:46 +0000

    LAST BUT NOT LEAST VERSION2

--- src/main/java/com/example/gestiontrip/model/TrajetProgrammer.java
+++ src/main/java/com/example/gestiontrip/model/TrajetProgrammer.java
@@ -17,40 +17,20 @@ import java.time.LocalTime;
 @AllArgsConstructor
 public class TrajetProgrammer {
     @Id
-    @GeneratedValue
-    @Column(name = "id_trajet_programmer")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idTrajetProgrammer;
 
-    @Column(name = "date_depart")
     private LocalDate dateDepart;
-
-    @Column(name = "heure_depart")
     private LocalTime heureDepart;
-
-    @Column(name = "date_arrivee_prevue")
     private LocalDate dateArriveePrevue;
-
-    @Column(name = "heure_arrivee_prevue")
     private LocalTime heureArriveePrevue;
 
-    @Column(name = "depart")
     private String depart;
-
-    @Column(name = "destination")
     private String destination;
-
-    @Column(name = "type_vehicule")
     private String typeVehicule;
-
-    @Column(name = "nb_passagers")
     private Short nbPassagers;
-
-    @Column(name = "autres_details")
     private String autresDetails;
 
-    @Column(name = "status")
     private boolean status = false;
-
-    @Column(name = "id_vehicule")
     private Long idVehicule;
 }
